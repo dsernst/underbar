551e0aa9dde0774f97a674d3de9d47c7eda34771
dsernst <git@dsernst.com>
2014-11-07 16:06:29 -0600

    pass last

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,11 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else {
+      return n === undefined ? array[array.length -1] : array.slice(-n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
