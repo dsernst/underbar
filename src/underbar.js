85650e54b42adb081da8966ca9cea7b2dd1067f1
dsernst <git@dsernst.com>
2014-11-07 23:14:34 -0600

    pass some

--- src/underbar.js
+++ src/underbar.js
@@ -243,6 +243,14 @@ var _ = {};
   // provided, provide a default one
   _.some = function (collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+
+    var reverseTest = function (item) {
+      return !iterator(item);
+    };
+    return !_.every(collection, reverseTest);
   };
 
 
